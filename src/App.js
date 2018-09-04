d08cc92d5ca84bec671777fbf001ecfb8aec0d31
Artem Beresnev <a.beresnev@runway-agency.ru>
2018-09-04 14:09:06 +0300

    fin first Parargaph

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+
+const list = [
+	{
+		title: "React",
+		url: "https://reactjs.org/",
+		author: "Jordan Walke",
+		num_comments: 3,
+		points: 4,
+		objectID: 0
+	},
+	{
+		title: "Redux",
+		url: "https://redux.js.org/",
+		author: "Dan Abramov, Andrew Clark",
+		num_comments: 2,
+		points: 5,
+		objectID: 1
+	}
+];
 
 class App extends Component {
-  render() {
-      var helloWorld = 'Добро пожаловать в путь к изучению React';
-      var user = {
-          name: "Artem",
-          surname: "Beresnev"
-      };
-    return (
-      <div className="App">
-        <h2>{helloWorld}</h2>
-          <p>Меня зовут {user.name} {user.surname}</p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				{list.map((item) =>
+					<div key={item.objectID}>
+							<span>
+								<a href={item.url}>{item.title}</a>
+							</span>
+						<span>{item.author}</span>
+						<span>{item.num_comments}</span>
+						<span>{item.points}</span>
+					</div>
+				)}
+			</div>
+		);
+	}
+}
+
+class Developer {
+	constuctor(firstname, lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	getName() {
+		return this.firstname + " " + this.lastname;
+	}
 }
 
 export default App;
